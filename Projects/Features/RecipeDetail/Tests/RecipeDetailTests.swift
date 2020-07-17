03f50cf9c1ccc6d74f08d08baea7a2719159e168
Marek Fořt <marek.fort@ackee.cz>
2020-07-17 16:03:49 +0200

    Add RecipeDetail tests.

--- Projects/Features/RecipeDetail/Tests/RecipeDetailTests.swift
+++ Projects/Features/RecipeDetail/Tests/RecipeDetailTests.swift
@@ -5,7 +5,40 @@ import ComposableTuistArchitectureSupport
 @testable import RecipeDetail
 
 final class RecipeDetailTests: XCTestCase {
-    func testExample() {
-        // Add your test here
+    let scheduler = DispatchQueue.testScheduler
+    
+    func testHappyFlow() {
+        let recipe: Recipe = .mock()
+        let store = setupStore(recipe: recipe)
+        store.assert(
+            .send(.changedRating(2)) {
+                $0.currentRating = 2
+            },
+            .send(.loadDetail) {
+                $0.hasLoadedDetail = true
+            },
+            .do { self.scheduler.advance() },
+            .receive(.recipeDetail(.success(recipe))),
+            .send(.changedRating(5)) {
+                $0.currentRating = 5
+            }
+        )
+    }
+    
+    // MARK: - Helpers
+    
+    private func setupStore(
+        recipe: Recipe = .mock()
+    ) -> TestStore<RecipeDetailState, RecipeDetailState, RecipeDetailAction, RecipeDetailAction, RecipeDetailEnvironment> {
+        TestStore(
+            initialState: RecipeDetailState(
+                recipe: recipe
+            ),
+            reducer: recipeDetailReducer,
+            environment: RecipeDetailEnvironment(
+                cookbookClient: .mock(),
+                mainQueue: AnyScheduler(self.scheduler)
+            )
+        )
     }
 }
